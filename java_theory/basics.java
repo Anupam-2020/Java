31b9d7eecd148a7f6f77acd96254dd11d6cc3243
Anupam-2020 <anupamanand.696@gmail.com>
2024-01-04 14:03:06 +0530

    added strings codes

--- java_theory/basics.java
+++ java_theory/basics.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class basics {
     public static void main(String[] args) {
         // float value = 2.4f;
@@ -10,5 +12,34 @@ public class basics {
         float l = 5.6f;
         int m = (int) l;
         System.out.println(m);
+
+        // switch statement
+        Scanner sc = new Scanner(System.in);
+        String days = sc.nextLine();
+
+        String result;
+
+        // M-1 traditional...
+        // switch (days) {
+        //     case "Monday":
+        //         result = "6am";
+        //         break;
+        //     case "Tuesday": 
+        //         result = "7am";
+        //         break;
+        //     default:
+        //         result = "8am";
+        //         break;
+        // }
+
+        // M-2 modern...
+        result = switch (days) {
+            case "Monday" -> "6am";
+            case "Tuesday" -> "7am";
+            case "Wednesday" -> "8am";
+            default -> "9am";
+        };
+
+        System.out.println(result);
     }
 }
