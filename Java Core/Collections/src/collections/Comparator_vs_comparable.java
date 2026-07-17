f9858c9b8c1de225ab3dd9fc215ec9b8adf6d408
Anupam-2020 <anupamanand.696@gmail.com>
2026-07-17 22:22:54 +0530

    added minor update

--- Java Core/Collections/src/collections/Comparator_vs_comparable.java
+++ Java Core/Collections/src/collections/Comparator_vs_comparable.java
@@ -1,6 +1,6 @@
 package collections;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Comparator_vs_comparable {
     public static void main(String[] args) {
@@ -16,6 +16,14 @@ public class Comparator_vs_comparable {
 
         Arrays.sort(cars, (Car obj1, Car obj2) -> obj2.carName.compareTo(obj1.carName)); // sorting in descending order based on carName using lambda expression. here we are using Comparator interface to sort the cars in descending order based on carName.
 
+
+        List<Car> carsList = new ArrayList<>();
+        carsList.add(new Car("SUV", "petrol"));
+        carsList.add(new Car("Sedan", "diesel"));
+        carsList.add(new Car("Hatchback", "CNG"));
+
+        Collections.sort(carsList, new CarNameComparator()); // This ia also an example of comparator, but by making classes for comparing objects...
+
         for(Car car: cars) {
             System.out.println(car.carName);
         }
@@ -25,7 +33,7 @@ public class Comparator_vs_comparable {
 }
 
 // Below is the Car class which implements Comparable interface and overrides compareTo method to sort the cars in descending order based on carName.
-class Car /* implements Comparator<Car> */  implements Comparable<Car>  { // Comparator vs Comparable: Comparator is used to sort the objects of a class based on different attributes and Comparable is used to sort the objects of a class based on a single attribute.
+class Car implements Comparable<Car> { // Comparator vs Comparable: Comparator is used to sort the objects of a class based on different attributes and Comparable is used to sort the objects of a class based on a single attribute.
     String carName;
     String carType;
 
@@ -35,13 +43,21 @@ class Car /* implements Comparator<Car> */  implements Comparable<Car>  { // Com
     }
 
 //    @Override
-//    public int compare(Car o1, Car o2) {
+//    public int compare(Car o1, Car o2) { // This is overridden from comparator functional interface.
 //        o1.carName.compareTo(o2.carName);
 //        return o1.carType.compareTo(o2.carType);
 //    }
 
     @Override
-    public int compareTo(Car o) {
+    public int compareTo(Car o) { // This is overridden from comparable functional interface.
         return o.carName.compareTo(this.carName); // here this refers to the current object and o refers to the object passed as parameter. So we are comparing the carName of the current object with the carName of the object passed as parameter. If we want to sort in ascending order then we can use this.carName.compareTo(o.carName).
+    }
+}
+
+class CarNameComparator implements Comparator<Car>{
+
+    @Override
+    public int compare(Car o1, Car o2) {
+        return o1.carType.compareTo(o2.carType);
     }
 }
